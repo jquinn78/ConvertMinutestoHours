062c61dbef756927b153d567e3f0cdf86ee68e66
jquinn78 <jquinn78@netzero.com>
2016-03-28 11:49:57 -0400

    Converted program to accept input from command line

--- main.go
+++ main.go
@@ -1,28 +1,38 @@
 // ConvertMinutesToHours project main.go
+
+/*
+Converts raw minutes into hours and minutes
+
+Usage: ./ConvertMinutesToHours <minutes>
+
+Input: Minutes from command line
+Output: H hours and M minutes
+
+On Error: Return "not enough args."
+*/
+
 package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
 
-	fmt.Print("Enter the number of minutes to convert: ")
-	var minutes float64
-	fmt.Scanf("%f", &minutes)
-
-	hours := minutes / 60
-
-	m := strings.SplitAfter(fmt.Sprintf("%f", hours), "4")
-	n := strings.SplitAfter(fmt.Sprintf("%f", hours), ".")
-
-	convMin, _ := strconv.ParseFloat(m[1], 2)
-	convH, _ := strconv.ParseFloat(n[0], 2)
-
-	math := convMin * 60
-
-	fmt.Printf("%v hours and %1.f Minutes\n", convH, math)
+	if len(os.Args) > 1 {
+		minArg := os.Args[1]
+		convArg, _ := strconv.ParseFloat(minArg, 2)
+		hours := convArg / 60
+		m := strings.Split(fmt.Sprintf("%f", hours), ".")
+		convH, _ := strconv.ParseFloat(m[0], 2)
+		decimal := hours - convH
+		math := decimal * 60
+		fmt.Printf("%v hours and %1.f Minutes\n", convH, math)
+	} else {
+		fmt.Println("Not enough args.")
+	}
 
 }
